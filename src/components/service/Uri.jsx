ea55e33ae87593c90b9220f0e87348e5e46581e5
chovatiya darshan <darshanchovatiya30@gmail.com>
2025-09-28 23:15:15 -1200

    set render path

--- src/components/service/Uri.jsx
+++ src/components/service/Uri.jsx
@@ -3,4 +3,4 @@
 // export const FrontendUrl = "http://localhost:5173"
 
 export const BaseUrl = "https://inventory-backend-ernu.onrender.com"
-export const FrontendUrl = "https://n39b4gh6-5173.inc1.devtunnels.ms"
+export const FrontendUrl = "https://inventory-frontend.vercel.app"
