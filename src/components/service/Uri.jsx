bcdc248a8568324a9a3dc4bfaba6e879d94e6505
chovatiya darshan <darshanchovatiya30@gmail.com>
2025-09-28 23:28:03 -1200

    vercel file add

--- src/components/service/Uri.jsx
+++ src/components/service/Uri.jsx
@@ -3,4 +3,4 @@
 // export const FrontendUrl = "http://localhost:5173"
 
 export const BaseUrl = "https://inventory-backend-ernu.onrender.com"
-export const FrontendUrl = "https://inventory-frontend.vercel.app"
+export const FrontendUrl = "https://inventory-system-ims.vercel.app"
