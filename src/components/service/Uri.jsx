1040644bc519e1b378d0e5f60b4cc5c8adaf0fc5
chovatiya darshan <darshanchovatiya30@gmail.com>
2025-09-28 23:14:55 -1200

    set render path

--- src/components/service/Uri.jsx
+++ src/components/service/Uri.jsx
@@ -1,6 +1,6 @@
 
-export const BaseUrl = "http://localhost:3000"
-export const FrontendUrl = "http://localhost:5173"
+// export const BaseUrl = "http://localhost:3000"
+// export const FrontendUrl = "http://localhost:5173"
 
-// export const BaseUrl = "https://n39b4gh6-3000.inc1.devtunnels.ms"
-// export const FrontendUrl = "https://n39b4gh6-5173.inc1.devtunnels.ms"
+export const BaseUrl = "https://inventory-backend-ernu.onrender.com"
+export const FrontendUrl = "https://n39b4gh6-5173.inc1.devtunnels.ms"
